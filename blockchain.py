99f4b06ef5f316d506dcfb574eb890c46ccc5b77
SoDone <shauraysingh08@gmail.com>
2021-05-11 03:52:58 -0400

    0000

--- blockchain.py
+++ blockchain.py
@@ -2,11 +2,14 @@ import hashlib
 import json
 from time import time
 
+from uuid import uuid4
+
 
 class Blockchain(object):
   def __init__(self):
     self.chain = []
     self.current_transaction = []
+
 
   def new_block(self):
     # Example of a block
@@ -41,4 +44,22 @@ class Blockchain(object):
   @property
   def last_block(self):
     pass
+
+
+  def proof_of_work(self, last_proof):
+    proof = 0
+    while self.valid_proof(last_proof, proof) is False:
+      proof += 1
+
+    return proof
+
+  @staticmethod
+  def valid_proof(last_proof, proof):
+    guess = f"{last_proof}{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:4] == "0000"
+
+
+
+
 
--- sol.py
+++ sol.py
@@ -7,3 +7,4 @@ while sha256(f'{x*y}'.encode()).hexdigest()[-1] != "0":
   y += 1
 
 print(f"the solution for y is {y}")
+print(hash(5*21))
