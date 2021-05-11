a20ebb973b96f5dea528816db3f26f6f118f9253
SoDone <shauraysingh08@gmail.com>
2021-05-11 03:32:32 -0400

    new_transaction

--- blockchain.py
+++ blockchain.py
@@ -4,10 +4,31 @@ class Blockchain(object):
     self.current_transaction = []
 
   def new_block(self):
+    # Example of a block
+    #block = {
+    #    'index': 1,
+    #    'timestamp': 1506057125.900785,
+    #    'transactions': [
+    #        {
+    #            'sender': "8527147fe1f5426f9dd545de4b27ee00",
+    #            'recipient': "a77f5cdfa2934df3954a5c7c7da5df1f",
+    #            'amount': 5,
+    #        }
+    #    ],
+    #    'proof': 324984774000,
+    #    'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+    #}
     pass
 
-  def new_transaction(self):
-    pass
+  def new_transaction(self, sender, recipient, amount):
+    
+    self.current_transactions.append({
+      "sender": sender,
+      "recipient": recipient,
+      "amount": amount,
+    })
+    return self.last_block['index'] + 1
+
 
   @staticmethod
   def hash(block):
@@ -16,3 +37,4 @@ class Blockchain(object):
   @property
   def last_block(self):
     pass
+
